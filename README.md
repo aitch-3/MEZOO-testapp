af3711c10310027f697941d6496321cc5727bb5b
Daniel Park <daniel@aitch3.com>
2019-04-08 17:22:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MEZOO-testapp
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+
+    overlayRect: any;
+
+    constructor(
+        private zone: NgZone
+    ) {
+
+    }
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.calculatePosition();
+        }, 100);
+    }
+
+    calculatePosition() {
+        this.zone.run(() => {
+            const el2 = document.getElementsByClassName('form_bg_overlay')[0];
+            this.overlayRect = el2.getBoundingClientRect();
+        });
+    }
 
 }
